66a67d666a93d8861fdb7a08644f48f5c02c499d
tianlinhe <tinaho_ok@hotmail.com>
2017-03-14 14:28:09 +0100

    Update week2_wloop.py

--- week2_wloop.py
+++ week2_wloop.py
@@ -20,7 +20,7 @@ def week_1(file_input, feature_output, aa_output):
         for p in range(len(f2o_line)):
             BE_list.append(f2o_line[p])
         print ("length of BE_list is", len(BE_list))
-    for w in range (1,2):
+    for w in range (1,24,2):
         with open(file_input, "r") as f_in:
             f1 = f_in.readlines()
             f3= open(aa_output,"w")
@@ -47,17 +47,17 @@ def week_1(file_input, feature_output, aa_output):
                 aa_insidelist.append(item[number])
             aa_outsidelist.append(aa_insidelist)
     #print (aa_outsidelist)
-        print ("length of aa_outsidelist is", len(aa_outsidelist))
-        #from sklearn import svm
-        #import numpy as np
-        #from sklearn.model_selection import cross_val_score
+        #print ("length of aa_outsidelist is", len(aa_outsidelist))
+        from sklearn import svm
+        import numpy as np
+        from sklearn.model_selection import cross_val_score
 
-        #clf = svm.SVC(kernel='rbf', C=1, random_state=314)
-        #clf.fit(aa_outsidelist, BE_list)
-        #scores = cross_val_score(clf, aa_outsidelist, BE_list, cv=5)
-        #print (w)
-        #print (scores)
-        #print ("Accuracy: %0.7f(+/- %0.6f)" %(scores.mean(), scores.std() * 2))
+        clf = svm.SVC(kernel='linear', C=1, random_state=314)
+        clf.fit(aa_outsidelist, BE_list)
+        scores = cross_val_score(clf, aa_outsidelist, BE_list, cv=5)
+        print (w)
+        print (scores)
+        print ("Accuracy: %0.7f(+/- %0.6f)" %(scores.mean(), scores.std() * 2))
 
 week_1("buried_exposed_beta.3line.txt", "BE.txt", "aa.txt")
 
